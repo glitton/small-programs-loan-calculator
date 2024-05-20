fee30e26e3434dbb34e14781c65cd10f0a816f17
Generosa Litton <glcodeworks@gmail.com>
2024-05-20 17:07:19 -0500

    launch school version

--- loanCalculator.js
+++ loanCalculator.js
@@ -1,101 +1,62 @@
-const input = require("readline-sync");
-const MESSAGES = require("./loan_calculator_messages.json");
+const readline = require("readline-sync");
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function invalidNumber(number) {
+function isInvalidNumber(number) {
   return (
-    number.trimStart() === "" ||
-    Number.isNaN(Number(number)) ||
-    Number(number) <= 0
+    number.trim() === "" || Number(number) < 0 || Number.isNaN(Number(number))
   );
 }
 
-function calculateMonthlyPayment(
-  loanAmount,
-  annualPercentageRate,
-  loanDurationInYears
-) {
-  let loanDurationInMonths = Number(loanDurationInYears) * 12;
-  let monthlyInterestRate = Number(annualPercentageRate) / 100 / 12;
-
-  let monthlyPayment =
-    Number(loanAmount) *
-    (monthlyInterestRate /
-      (1 - Math.pow(1 + monthlyInterestRate, -loanDurationInMonths)));
-  monthlyPayment = monthlyPayment.toFixed(2);
-
-  return monthlyPayment;
-}
-
-/* ---- START OF PROGRAM ----- */
-
-prompt(MESSAGES["welcome"]);
-prompt(MESSAGES["description"]);
+prompt("Welcome to Mortgage Calculator!");
 
 while (true) {
-  prompt(MESSAGES["loanAmount"]);
-  let loanAmount = input.question();
-  while (invalidNumber(loanAmount)) {
-    prompt(MESSAGES["invalidNumber"]);
-    loanAmount = input.question();
+  prompt("---------------------------------");
+
+  prompt("What is the loan amount?");
+
+  let amount = readline.question();
+  while (isInvalidNumber(amount)) {
+    prompt("Must enter a positive number");
+    amount = readline.question();
   }
 
-  prompt(MESSAGES["annualPercentageRate"]);
-  let annualPercentageRate = input.question();
-  while (invalidNumber(annualPercentageRate)) {
-    prompt(MESSAGES["invalidNumber"]);
-    annualPercentageRate = input.question();
+  prompt("What is the interest rate?");
+  prompt("(Example: 5 for 5% or 2.5 for 2.5%)");
+  let interestRate = readline.question();
+
+  while (isInvalidNumber(interestRate)) {
+    prompt("Must enter a positive number");
+    interestRate = readline.question();
   }
 
-  prompt(MESSAGES["loanDurationInYears"]);
-  let loanDurationInYears = input.question();
-  while (invalidNumber(loanDurationInYears)) {
-    prompt(MESSAGES["invalidNumber"]);
-    loanDurationInYears = input.question();
+  prompt("What is the loan duration (in years)?");
+  let years = readline.question();
+
+  while (isInvalidNumber(years)) {
+    prompt("Must enter a positive number");
+    years = readline.question();
   }
 
-  prompt(`You entered:\n
- - a loan amount of $${loanAmount} dollars\n
- - with an annual percentage rate of ${annualPercentageRate} percent\n
- - and a loan term of ${loanDurationInYears} years.\n
- If your input is correct, press 'y' to continue.  To start over press 'n'`);
+  let annualInterestRate = Number(interestRate) / 100;
+  let monthlyInterestRate = annualInterestRate / 12;
+  let months = Number(years) * 12;
 
-  let userInput = input.question();
-  while (!["y", "n"].includes(userInput)) {
-    prompt(MESSAGES["invalidInput"]);
-    userInput = input.question();
+  let monthlyPayment =
+    Number(amount) *
+    (monthlyInterestRate /
+      (1 - Math.pow(1 + monthlyInterestRate, -Number(months))));
+
+  prompt(`Your monthly payment is: $${monthlyPayment.toFixed(2)}`);
+
+  prompt("Another calculation?");
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== "n" && answer[0] !== "y") {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
   }
 
-  console.clear();
-
-  if (userInput === "n") continue;
-
-  let userMonthlyPayment = calculateMonthlyPayment(
-    loanAmount,
-    annualPercentageRate,
-    loanDurationInYears
-  );
-
-  prompt(`A loan amount of $${loanAmount} dollars\n 
-  - with an annual percentage rate of ${annualPercentageRate} percent\n
-  - and a loan term of ${loanDurationInYears} years means that:\n`);
-  prompt(`${MESSAGES["monthlyPayment"]}$${userMonthlyPayment}`);
-  prompt(MESSAGES["anotherCalculation"]);
-
-  let answer = input.question();
-  console.clear();
-
-  while (!["y", "x"].includes(answer)) {
-    prompt(MESSAGES["choice"]);
-    answer = input.question();
-    console.clear();
-  }
-
-  if (answer !== "y") {
-    console.clear();
-    break;
-  }
+  if (answer[0] === "n") break;
 }
